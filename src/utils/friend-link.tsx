5dd684b0052de764bbdec252bbe5e8af8618218a
@Always <zhxy_chn@126.com>
2025-05-18 20:10:33 +0800

    代码重构：友情链接页面代码重构并优化样式
    1.原因分析：无
    2.修改方法：无

--- src/utils/friend-link.tsx
+++ src/utils/friend-link.tsx
@@ -1,28 +1,40 @@
-import { escapeHTML } from './escape-html';
-import { h } from './jsx-runtime';
+import {escapeHTML} from './escape-html';
+import {h} from './jsx-runtime';
 
 export function make_friends_list() {
-  try {
-    const friendHTML = ({ avatar, href, title, description }: any) => (
-      <div class="friend-link-container">
-        <div class="friend-link-box">
-          <aside class="friend-link-avatar">
-            <img src={escapeHTML(avatar)} href={escapeHTML(href)} />
-          </aside>
-          <div class="friend-link-meta">
-            <div class="friend-link-title">
-              <a href={escapeHTML(href)}>{escapeHTML(title)}</a>
-            </div>
-            <div class="friend-link-description">{escapeHTML(description)}</div>
-          </div>
-        </div>
-      </div>
-    );
+    try {
+        const getRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+        const friendHTML = ({avatar, href, title, description}: any) => (
 
-    document.querySelectorAll('.friend-link').forEach((friend) => {
-      friend.replaceWith(friendHTML((friend as HTMLElement).dataset));
-    });
-  } catch (err) {
-    console.error(err);
-  }
+            <div class="friend-link-card">
+                <aside class="friend-link-avatar">
+                    {avatar ? (
+                        <img
+                            src={escapeHTML(avatar)}
+                            onerror={(e) => {
+                                const color = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+                                e.target.replaceWith(<div class="avatar-placeholder"
+                                                          style={`background-color: ${color}`}/>);
+                            }}
+                        />
+                    ) : (
+                        <div class="avatar-placeholder" style={`background-color: ${getRandomColor()}`}/>
+                    )}
+                </aside>
+                <div class="friend-link-content">
+                    <div class="friend-link-title">
+                        <a href={escapeHTML(href)}>{escapeHTML(title)}</a>
+                    </div>
+                    <div class="friend-link-description">{escapeHTML(description)}</div>
+                </div>
+            </div>
+
+        );
+
+        document.querySelectorAll('.friend-link').forEach((friend) => {
+            friend.replaceWith(friendHTML((friend as HTMLElement).dataset));
+        });
+    } catch (err) {
+        console.error(err);
+    }
 }
